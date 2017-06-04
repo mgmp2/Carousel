bc0f1adc82077d7003ffc995212746af8efd8659
mgmp2 <miriammp1997@gmail.com>
2017-06-04 01:12:40 -0500

    function btn_sliders to move the img

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,49 @@
 $( () => {
+    var firstImg = 1;
     var slider = $("#carousel");
+    var lyst = $("#carousel .slider__listas");
+    var circle = $(".btn__slider");
 
+    showImg(firstImg);
     $("#btn-next").on('click',function(e) {
         e.preventDefault();
-        $('#carousel .slider__listas:first').insertAfter('#carousel .slider__listas:last');
-        slider.css('margin-left', '-'+0+'px');
+        moveImg(1);
     });
     $("#btn-prev").on('click',function(e) {
         e.preventDefault();
-        $('#carousel .slider__listas:last').insertBefore('#carousel .slider__listas:first');
-        slider.css('margin-left', '-'+0+'px');
+        moveImg(-1);
     });
+    function showImg(valor){
+      lyst.hide();
+      lyst.eq(valor).show();
+      circle.removeClass("color-circle");
+      circle.eq(valor).addClass("color-circle");
+      firstImg = valor + 1;
+    }
 
-    $('#botones .btn__slider').on('click', (e) => {
-        e.preventDefault();
-        alert("Bienvenido: "+e);
-
-        console.log(e);
-    });
-
+    function moveImg(valor){
+      firstImg += valor;
+      if(firstImg > lyst.length){
+        firstImg = 1;
+      }
+      if(firstImg < 1){
+        firstImg = lyst.length;
+      }
+      showImg(firstImg-1);
+    }
+    // SLIDER CIRCLE
+    circle.on("click",function(){
+        switch (this.id) {
+          case "first":  showImg(0);
+            break;
+          case "second": showImg(1);
+            break;
+          case "third":  showImg(2);
+            break;
+          case "fourth":  showImg(3);
+            break;
+          case "fifth":  showImg(4);
+            break;
+        }
+      });
 })
